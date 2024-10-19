096c9e8cc6f1588f579fb0f69c49fa89b5d9140f
danichagas <chagasdaniel788@gmail.com>
2024-10-19 15:18:11 -0300

    feat: prompt configuration file

---
+++ src/prompts/prompt.js
@@ -0,0 +1,9 @@
+const mainPrompt = [
+  {
+    name: "select",
+    description: "Selecione 1 para gerar o seu QRcode",
+    pattern: /^[1]+$/,
+    message: "Escolha apenas o número 1",
+    required: true
+  }
+]
