c990bf6e7160ec9303c04b82d20dcd54740a14fb
danichagas <chagasdaniel788@gmail.com>
2024-10-20 00:41:31 -0300

    feat: main prompt

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import prompt from "prompt"
+import mainPrompt from "./prompts/prompt.js"
 
-async function main() {}
+async function main() {
+  prompt.get(mainPrompt, async (err, result) => {
+    if (result.select == 1) console.log("QRcode sendo gerado")
+  })
+
+  prompt.start()
+}
 main()
