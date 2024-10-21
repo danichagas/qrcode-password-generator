a66ffe529f08108f1ba678089eab2ccb03b2f998
danichagas <chagasdaniel788@gmail.com>
2024-10-21 16:29:15 -0300

    feat: Qrcode function using

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import prompt from "prompt"
 import mainPrompt from "./prompts/prompt.js"
+import createQrcode from "./services/qrcode/create.js"
 
 async function main() {
   prompt.get(mainPrompt, async (err, result) => {
-    if (result.select == 1) console.log("QRcode sendo gerado")
+    if (result.select == 1) await createQrcode()
   })
 
   prompt.start()
