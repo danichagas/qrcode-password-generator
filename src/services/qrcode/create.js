f4c87b62ffdd1532e21392befa6033983697da13
danichagas <chagasdaniel788@gmail.com>
2024-10-22 01:31:59 -0300

    feat: "handle" function using

--- src/services/qrcode/create.js
+++ src/services/qrcode/create.js
@@ -1,6 +1,11 @@
 import prompt from "prompt"
-import promptQrcode from "../../prompts/prompt-qrcode"
+import promptQrcode from "../../prompts/prompt-qrcode.js"
+import handle from "./handle.js"
 
-async function createQrcode() {}
+async function createQrcode() {
+  prompt.get(promptQrcode, handle)
+
+  prompt.start()
+}
 
 export default createQrcode
