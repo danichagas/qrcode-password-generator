bda9417801ab62e1f8ec56273abcc3dc4bd68ea0
danichagas <chagasdaniel788@gmail.com>
2024-10-22 01:27:35 -0300

    feat: schema prompt-qrcode create

--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -1,3 +1,15 @@
-const promptQrcode = []
+const promptQrcode = [
+  {
+    name: "link",
+    description: "Cole o link para gerar o QRcode", 
+  },
+  {
+    name: "type",
+    description: "Escolha entre o tipo (1- NORMAL ou 2- TERMINAL",
+    pattern: /^[1-2]+$/,
+    message: "Escolha apenas entre 1 e 2",
+    required: true
+  }
+]
 
 export default promptQrcode
